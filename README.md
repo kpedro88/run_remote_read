0ad11f5d117a5f936463b7dd3b50b01265e8ecf1
Kevin Pedro <kpedro88@gmail.com>
2025-11-06 17:08:05 -0600

    first commit of simple test setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# Run Remote Read (RRR)
+
+A simple repository to submit test jobs that run remote reads,
+in order to test the stability and reliability of the AAA (any data, any time, anywhere) data federation using `xrootd`.
+
+## Usage
+
+```bash
+condor_submit test.jdl
+```
---
+++ test.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -e
+
+echo "Starting job on "`date` # to display the start date
+echo "Running on "`uname -a` # to display the machine where the job is running
+echo "System release "`cat /etc/redhat-release` # and the system release
+echo "CMSSW on Condor"
+
+# check arguments
+export CMSSWVER=""
+export LFN=""
+export REDIR=""
+while getopts "C:L:X" opt; do
+	case "$opt" in
+		C) CMSSWVER="$OPTARG"
+		;;
+		L) LFN="$OPTARG"
+		;;
+		X) REDIR="$OPTARG"
+		;;
+	esac
+done
+
+# standardize values
+REDIR="${REDIR%/}/"
+
+echo ""
+echo "parameter set:"
+echo "CMSSWVER: $CMSSWVER"
+echo "LFN: $LFN"
+echo "REDIR: $REDIR"
+echo ""
+
+# environment setup
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+# get CMSSW (for xrootd and root)
+cmsrel ${CMSSWVER}
+cd ${CMSSWVER}/src
+cmsenv
+
+# create macro
+cat << EOF > test.C
+auto file = TFile::Open("$REDIR$LFN");
+file->ls();
+auto tree = (TTree*)file->Get("events");
+tree->Print();
+EOF
+
+# try to read the file
+root -b -l -q test.C
+
+echo ""
+echo "Done"
